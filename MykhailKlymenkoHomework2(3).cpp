61c4685c2cb84d1d73af8b8163c40d73f273e803
Mykhail Klymenko <scottishmike1995@gmail.com>
2021-04-05 11:45:20 +0300

    UpdatedMykhailKlymenkoHomework2(3)

--- MykhailKlymenkoHomework2(3).cpp
+++ MykhailKlymenkoHomework2(3).cpp
@@ -4,32 +4,34 @@
 
 int main() {
     int NumberTicket{};
+    std::cout << "This program verifies a lucky ticket." << std::endl;
     std::cout << "Enter 6 numbers of your ticket: ";
     std::cin >> NumberTicket;
-    const int MinNumber{ 100000 }, MaxNumber{ 999999 };
+    constexpr int MinNumber{ 100000 }, MaxNumber{ 999999 };
     while (NumberTicket < MinNumber || NumberTicket > MaxNumber) {
         std::cout << "The number is not in a range!\n";
         std::cin >> NumberTicket;
     }
     int LastDigit{ NumberTicket % 10 };
     int i{}, sum1{}, sum2{};
+    constexpr int lim1{ 0 }, lim2{ 4 }, lim3{ 3 }, lim4{ 7 };
 
     while (LastDigit != 0) {
         i++;
-        if (i > 0 && i < 4) {
+        if (i > lim1 && i < lim2) {
             sum1 += LastDigit;
         }
-        if (i > 3 && i < 7) {
+        if (i > lim3 && i < lim4) {
             sum2 += LastDigit;
         }
         NumberTicket = (NumberTicket - LastDigit) / 10;
         LastDigit = NumberTicket % 10;
     }
-    if (sum1 == sum2) {
-        std::cout << "Go nuts, you've got a lucky ticket!!!)" << std::endl;
-    }
-    else {
-        std::cout << "Sorry, you're not a winner! Good luck next time" << std::endl;
-    }
+
+    sum1 == sum2
+        ? std::cout << "Go nuts, you've got the lucky ticket!!!)" << std::endl
+        : std::cout << "Sorry, you're not a winner! Good luck next time"
+        << std::endl;
+
     return 0;
 }
